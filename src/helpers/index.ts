d76b09252a120724c3e83e89b31165b5a0e0fcdc
orianderson <and.orisistem@gmail.com>
2022-12-17 12:47:30 -0300

    feat: implement status code object

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
 export * from './Replace';
 export * from './set-access-headers';
+export * from './status-code-response';
--- src/infra/controllers/register-user.controller.ts
+++ src/infra/controllers/register-user.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post, Inject } from '@nestjs/common';
+import { Body, Controller, Post, Inject, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 import { UseCaseProxy } from '@infra/usecases-proxy';
 import { RegisterUser } from '@app/usecases';
 import { RegisterUserBody } from './dto';
 import { UserViewModel } from './presenters';
+import { StatusCodeResponse } from '@helpers/index';
 
 import { UsecasesProxyModule } from '@infra/usecases-proxy';
 
@@ -15,10 +17,12 @@ export class UsersController {
   ) {}
 
   @Post()
-  async create(@Body() body: RegisterUserBody) {
+  async create(@Body() body: RegisterUserBody, @Res() res: Response) {
     const repository = this.usersRepository.getInstance();
     const user = await repository.execute(body);
 
-    return UserViewModel.toHttpResponse(user);
+    res
+      .status(StatusCodeResponse.CREATED)
+      .send(UserViewModel.toHttpResponse(user));
   }
 }
