a05cf7417eef07c5845dac424b49af65d8495d50
orianderson <and.orisistem@gmail.com>
2023-02-28 10:02:09 -0300

    refactor: User entity

--- src/domain/entities/user.ts
+++ src/domain/entities/user.ts
@@ -9,31 +9,26 @@ import {
 } from '../errors';
 import { Either, left, right } from '../../helpers';
 
-export class User {
-  public readonly user: {
-    id: string;
-    username: string;
-    email: Email;
-    name: Name;
-    profile: string;
-    password: Password;
-  };
+interface UserProps {
+  id: string;
+  username: string;
+  email: Email;
+  name: Name;
+  profile: string;
+  password: Password;
+}
 
-  private constructor(
-    id: string,
-    email: Email,
-    name: Name,
-    password: Password,
-    username: string,
-    profile: string,
-  ) {
+export class User {
+  public readonly user: UserProps;
+
+  private constructor(user: UserProps) {
     this.user = {
-      id: id ?? User.generateId(),
-      email: email,
-      name: name,
-      username: username,
-      profile: profile,
-      password: password,
+      id: user.id ?? User.generateId(),
+      email: user.email,
+      name: user.name,
+      username: user.username,
+      profile: user.profile,
+      password: user.password,
     };
   }
 
@@ -59,14 +54,14 @@ export class User {
     }
 
     return right(
-      new User(
-        newUser.id,
-        email.value as Email,
-        name.value as Name,
-        password.value as Password,
-        newUser.username,
-        newUser.profile,
-      ),
+      new User({
+        id: newUser.id,
+        email: email.value as Email,
+        name: name.value as Name,
+        password: password.value as Password,
+        username: newUser.username,
+        profile: newUser.profile,
+      }),
     );
   }
 
